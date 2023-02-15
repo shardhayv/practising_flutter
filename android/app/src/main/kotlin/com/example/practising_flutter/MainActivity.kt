b2468579c06ead6e727ebc68309494d3fc2db195
shardhayv <shardhayvatshyayan7@gmail.com>
2023-02-15 09:05:12 +0545

    started basic project

---
+++ android/app/src/main/kotlin/com/example/practising_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.practising_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
+        title: const Center(child: Text('I am Rich')),
+        backgroundColor: Colors.black,
+      ),
+    ),
+  ));
+}
