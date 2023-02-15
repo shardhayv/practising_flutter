5a90454ea4692825142dfc31f9e9e482fc8cc567
shardhayv <shardhayvatshyayan7@gmail.com>
2023-02-15 09:14:44 +0545

    Network Image added

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,11 @@ void main() {
         title: const Center(child: Text('I am Rich')),
         backgroundColor: Colors.black,
       ),
+      body: const Center(
+          child: Image(
+        image: NetworkImage(
+            'https://images.unsplash.com/photo-1676321228272-0cccce03e290?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80'),
+      )),
     ),
   ));
 }
