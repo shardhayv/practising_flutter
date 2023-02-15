c37171b75185a8c20cd3b4fef088fdd0bf1a32e3
shardhayv <shardhayvatshyayan7@gmail.com>
2023-02-15 09:28:46 +0545

    Asset Image added

--- lib/main.dart
+++ lib/main.dart
@@ -10,8 +10,7 @@ void main() {
       ),
       body: const Center(
           child: Image(
-        image: NetworkImage(
-            'https://images.unsplash.com/photo-1676321228272-0cccce03e290?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1964&q=80'),
+        image: AssetImage('images/oswaldo-delgado-wV8EOV3LEtU-unsplash.jpg'),
       )),
     ),
   ));
